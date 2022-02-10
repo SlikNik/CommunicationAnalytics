ac95ec69eb206c93d8d2e0f8970164ee9b1ab5a2
Nikal Morgan <nikalmorgan11@gmail.com>
2022-02-10 01:59:09 -0600

    updated controller

--- analytics/src/app.controller.ts
+++ analytics/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { EventPattern, MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 import { CreateUserEvent } from './create-user.event';
