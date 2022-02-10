49bce11daf26fcc91df040ce909844b9f1304aeb
Nikal Morgan <nikalmorgan11@gmail.com>
2022-02-10 01:04:52 -0600

    created event; updated controller and service

--- communication/src/app.controller.ts
+++ communication/src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
+import { EventPattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import { CreateUserEvent } from './create-user.event';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @EventPattern('user_created')
+  handleUserCreated(data: CreateUserEvent) {
+    this.appService.handleUserCreated(data);
   }
 }
--- communication/src/app.service.ts
+++ communication/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import console from 'console';
+import { CreateUserEvent } from './create-user.event';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  handleUserCreated(data: CreateUserEvent) {
+    console.log('handleUserCreated - COMMUNICATIONS', data);
+    //TODO: Email User......
   }
 }
