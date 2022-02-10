36f1bc6ce12f29d95fb7507fd67628a9f9b1b373
Nikal Morgan <nikalmorgan11@gmail.com>
2022-02-10 01:41:09 -0600

    hybrid microservices: created event; updated controller, service, and main

--- analytics/src/app.service.ts
+++ analytics/src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserEvent } from './create-user.event';
 
 @Injectable()
 export class AppService {
+  private readonly analytics: any[] = [];
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  handleUserCreated(data: CreateUserEvent): void {
+    console.log('handleUserCreated - ANALYTICS', data);
+    this.analytics.push({
+      email: data.email,
+      timestamp: new Date(),
+    });
   }
 }
